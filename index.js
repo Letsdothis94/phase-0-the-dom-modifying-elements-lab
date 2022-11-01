fd97f8e0e37ec5cb36adb29c632464ff4589a8f6
Cristian Contreras <contreras.cristian994@gmail.com>
2022-11-01 19:52:25 -0400

    commit tree d550e75ff5ae6075eccc4e946b63cf78badcdfa5

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Cristian is the champion";
+
+
+
+
+
+
+
+
+
+
+
+
+
